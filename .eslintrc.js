e6b21b0344c9abcb273ede663f92034e8a6a010f
Stefan Ylimäki <stefan.ylimaki@gmail.com>
2023-01-23 15:55:23 +0200

    Added and configured eslint

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  'env': {
+    'es2021': true,
+    'node': true,
+    'react-native/react-native': true
+  },
+  'plugins': [
+    'react', 'react-native'
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'ecmaFeatures': {
+      'jsx': true
+    },
+    'sourceType': 'module'
+  },
+  'rules': {
+    'react-native/no-unused-styles': 2,
+    'react-native/split-platform-components': 2,
+    'react-native/no-raw-text': 2,
+    'react-native/no-single-element-style-arrays': 2,
+    'indent': [
+      'error',
+      2
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ]
+  }
+}
