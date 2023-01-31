ef3228574313803a13b73ce6462f38f71d9e2271
Stefan Ylimäki <stefan.ylimaki@gmail.com>
2023-01-31 11:53:36 +0200

    Implemented Tab Navigation on top level, stack navigation in tab-level. To make application more or less match consepts, navigation-wise.

---
+++ screens/AllSubstitutions.js
@@ -0,0 +1,16 @@
+import { Text, View } from 'react-native'
+import React from 'react'
+
+import SubstitutionsList from '../components/SubstitutionsList'
+import substitutions from '../assets/data/substitutionsData.json'
+
+const AllSubstitutions = ({ navigation }) => {
+  return (
+    <View>
+      <Text>Tässä listassa ovat kaikki sijaisuudet.</Text>
+      <SubstitutionsList navigation={navigation} substitutions={substitutions}/>
+    </View>
+  )
+}
+
+export default AllSubstitutions
