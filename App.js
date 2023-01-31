8c4ee39234e40458d0b6544ebbb579e8a32d7c51
Jaakko Rantamäki <rantamaki@live.fi>
2023-01-31 17:34:37 +0200

    Navigoinnin pelkistys

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-import { NavigationContainer } from '@react-navigation/native'
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
+import {NavigationContainer} from '@react-navigation/native'
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs'
 import {SafeAreaView, StatusBar, StyleSheet} from 'react-native'
 
 import SavedSubstitutionsStack from './screens/SavedSubstitutionsStack'
@@ -10,23 +10,39 @@ import TailoredSubstitutionsStack from './screens/TailoredSubstitutionsStack'
 
 const Tab = createMaterialTopTabNavigator()
 export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar/>
-      <NavigationContainer>
-        <Tab.Navigator >
-          <Tab.Screen name="Tykätyt" component={SavedSubstitutionsStack} />
-          <Tab.Screen name="Sinulle" component={TailoredSubstitutionsStack} />
-          <Tab.Screen name="Kaikki" component={AllSubstitutionsStack} />
-        </Tab.Navigator>
-      </NavigationContainer>
-    </SafeAreaView>
-  )
+    return (
+        <SafeAreaView style={styles.container}>
+            <StatusBar/>
+            <NavigationContainer>
+                <Tab.Navigator
+                    screenOptions={{
+                        swipeEnabled: false,
+                        tabBarContentContainerStyle: {
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                        },
+                        tabBarItemStyle: {
+                            width: 'auto',
+                            position: 'relative',
+                        },
+                        tabBarIndicatorStyle: {
+                            display: 'none',
+                        },
+                    }}
+                >
+
+                    <Tab.Screen name="Tykätyt" component={SavedSubstitutionsStack}/>
+                    <Tab.Screen name="Tailored" component={TailoredSubstitutionsStack} options={{ title: 'Sinulle' }}/>
+                    <Tab.Screen name="Kaikki" component={AllSubstitutionsStack}/>
+                </Tab.Navigator>
+            </NavigationContainer>
+        </SafeAreaView>
+    )
 };
 
 //väliaikaisesti varmaan nyt näin, keksitään jokin
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
+    container: {
+        flex: 1,
+    },
 })
