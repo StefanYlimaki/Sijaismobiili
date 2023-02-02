438609ea6689751b31e1c0bf19950355d307ed51
Eetu <eetu.seppanen99@gmail.com>
2023-02-02 21:41:28 +0200

    List can now be browsed by swiping

--- screens/RecommendationView.js
+++ screens/RecommendationView.js
@@ -1,22 +1,52 @@
 import { Component, StyleSheet, Text, View, Dimensions, Image, Animated, PanResponder } from 'react-native'
 import React, {useRef, useState} from 'react'
-
 import substitutions from '../assets/data/substitutionsData_new.json'
 import styles from '../assets/styles/styles'
 
 const SCREEN_HEIGHT = Dimensions.get('window').height
 const SCREEN_WIDTH = Dimensions.get('window').width
+const SWIPE_THRESHOLD = 120
 
 const renderSubstitutions = () => {
   const position = useRef(new Animated.ValueXY()).current
-  const [currentIndex] = useState(0)
+  const [currentIndex, incrementIndex] = useState(0)
   const panResponder = useRef(
     PanResponder.create({
       onMoveShouldSetPanResponder: () => true,
       onStartShouldSetPanResponder: () => true,
       onPanResponderMove: Animated.event([null, {dx: position.x,
         dy: position.y}], {useNativeDriver: false}),
-      onPanResponderRelease: () => {
+      onPanResponderRelease: (evt, gestureState) => {
+        //Kiinnittäydy
+        if (gestureState.dx > SWIPE_THRESHOLD) {
+          Animated.spring(position, {
+            toValue: {x: SCREEN_WIDTH + 100, y: gestureState.dy},
+            useNativeDriver: false
+          }
+          ).start(() => {
+            incrementIndex(prevIndex => prevIndex + 1)
+            position.setValue({x: 0, y: 0})
+          })
+
+        //Kieltäydy
+        } else if (gestureState.dx < -SWIPE_THRESHOLD) {
+          Animated.spring(position, {
+            toValue: {x: -SCREEN_WIDTH - 100, y: gestureState.dy},
+            useNativeDriver: false
+          }
+          ).start(() => {
+            incrementIndex(prevIndex => prevIndex + 1)
+            position.setValue({x: 0, y: 0})
+          })
+
+        //Palauta kortti alkuasemaan
+        } else {
+          Animated.spring(position, {
+            toValue: {x: 0, y: 0},
+            friction: 4,
+            useNativeDriver: false
+          }).start()
+        }
 
       }
     })
@@ -35,6 +65,18 @@ const renderSubstitutions = () => {
       {translateY: position.y}
     ]
   }
+
+  const nextCardOpacity = position.x.interpolate({
+    inputRange: [-SCREEN_WIDTH/2, 0, SCREEN_WIDTH / 2],
+    outputRange: [1, 0, 1],
+    extrapolate: 'clamp'
+  })
+
+  const nextCardScale = position.x.interpolate({
+    inputRange: [-SCREEN_WIDTH/2, 0, SCREEN_WIDTH],
+    outputRange: [1, 0.8, 1],
+    extrapolate: 'clamp'
+  })
 
   return substitutions.map((item, i) => {
     if (i < currentIndex) {
@@ -74,6 +116,8 @@ const renderSubstitutions = () => {
           key={i}
           style={{
             height: SCREEN_HEIGHT - 120,
+            opacity: nextCardOpacity,
+            transform: [{scale: nextCardScale}],
             width: SCREEN_WIDTH,
             padding: 10,
             position: 'absolute',
