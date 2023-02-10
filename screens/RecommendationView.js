baa33ef0ffc72ed4bf2dc86bcbea36216fe623d0
EetuSeppa <75948130+EetuSeppa@users.noreply.github.com>
2023-02-10 13:15:20 +0200

    Recommendation View styles

--- screens/RecommendationView.js
+++ screens/RecommendationView.js
@@ -1,4 +1,5 @@
-import { Component, StyleSheet, Text, View, Dimensions, Image, Animated, PanResponder } from 'react-native'
+import { Pressable, Component, StyleSheet, Text, View, Dimensions, Image, Animated, PanResponder } from 'react-native'
+
 import React, {useRef, useState} from 'react'
 import substitutions from '../assets/data/substitutionsData_new.json'
 import styles from '../assets/styles/styles'
@@ -111,6 +112,27 @@ const renderSubstitutions = () => {
     if (i < currentIndex) {
       return null
     } else {
+
+      const benefits = item.benefits.map((benefit, i) => {
+        return (
+          <View style={[styles.substitutionItemBenefitsItem, {
+            width:'30%',
+            flexDirection: 'column'
+
+          }
+          ]}
+          key={i}
+          >
+            <Text
+              //style={{alignSelf: 'flex-end'}}
+            >
+              {benefit}
+            </Text>
+          </View>
+        )
+      })
+
+
       return (
         <Animated.View 
           key={i}
@@ -126,34 +148,158 @@ const renderSubstitutions = () => {
             {
               height: SCREEN_HEIGHT - 120,
               width: SCREEN_WIDTH,
-              padding: 10,
               position: 'absolute',
-              backgroundColor: krBlue,
+              backgroundColor: 'white',
               borderRadius: 20,
               resizeMode: 'cover'
             }
           ]}
         >
-          <View>
-            <Text>
+          <View style={{
+            flexDirection: 'column',
+            alignItems: 'flex-end',
+            height: '15%',
+            paddingTop: 20
+          }}>
+            {benefits}
+          </View>
+          <View style={{
+            flexDirection: 'column',
+            paddingLeft: 10
+          }}>
+            <Text style={{
+              fontWeight: 'bold',
+              fontSize: 30
+            }}>
               {item.title}
             </Text>
-            <Text>
+            <Text style={{
+              fontSize: 20
+            }}>
               {item.department}
             </Text>
-            <Text>
+
+          </View>
+          <View style={{
+            marginTop: 20,
+            padding: 10,
+            backgroundColor: krBlue,
+            flexDirection: 'column',
+          }}>
+            <Text style={{
+              color: 'white',
+              alignSelf: 'flex-start',
+              fontSize: 13
+            }}>
+              {parseDate(item.date)} 
+            </Text>
+            <Text style={{
+              color: 'white',
+              alignSelf: 'flex-start',
+              fontSize: 13
+            }}>
+              {parseTime(item.date, item.timing.duration)} 
+            </Text>
+            <Text style={{
+              color: 'white',
+              alignSelf: 'flex-end',
+              fontSize: 13
+            }}>
               {item.organisation}
             </Text>
-          </View>
-          <View>
-            <Text>
-              {item.location}
+            <Text style={{
+              color: 'white',
+              alignSelf: 'flex-end',
+              fontSize: 13
+            }}>
+              2 km
             </Text>
+          </View>
+
+          <View style={{
+            flexDirection: 'row',
+            justifyContent: 'flex-end',
+            flexWrap: 'nowrap',
+            padding: 10
+          }}>
+            <Text style={{fontWeight:'bold', textAlign:'right'}}>
+              {item.hourlyPay + '€/h'}
+            </Text>
+            <Text>
+              {'(~' + Math.floor(item.hourlyPay * (item.timing.duration/60)) + '€)'}
+            </Text>
+
+          </View>
+
+          <View style={{
+            paddingTop: '60%',
+            paddingLeft: 20,
+            paddingRight: 20,
+            flexDirection: 'row',
+            justifyContent: 'space-between'
+          }}>
+            <Pressable style={{
+              width: '30%'
+            }}
+            onPress={() => {
+              incrementIndex(prevIndex => prevIndex + 1)
+            }}
+            >
+              <View style={{
+                backgroundColor: '#91041D',
+                width: 100,
+                height: 100,
+                borderRadius: 50
+              }}>
+              </View>
+            </Pressable>
+
+            <Pressable style={{
+              width: '30%'
+            }}
+            >
+              <View style={{
+                backgroundColor: '#0666DB',
+                width: 100,
+                height: 100,
+                borderRadius: 50
+              }}>
+              </View>
+            </Pressable>
+
+            <Pressable style={{
+              width: '30%'
+            }}
+            onPress={() => {
+              incrementIndex(prevIndex => prevIndex + 1)
+            }}
+            >
+              <View style={{
+                backgroundColor: '#13912A',
+                width: 100,
+                height: 100,
+                borderRadius: 50
+              }}>
+              </View>
+            </Pressable>
           </View>
         </Animated.View>
       )
     }
   }).reverse()
+}
+
+const parseDate = (rawDate) => {
+  const date = new Date(rawDate)
+  return (date.toDateString())
+}
+
+const parseTime = (rawDate, duration) => {
+  const startDate = new Date(rawDate)
+  const endDate = new Date(startDate.getTime() + duration*60000)
+
+  
+  return startDate.toLocaleTimeString() + '-' + endDate.toLocaleTimeString()
 }
 
 export default RecommendationView
