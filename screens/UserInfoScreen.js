8e7fb1735162abfdb180f6a28d39c10c4b9dfae1
Stefan Ylimäki <stefan.ylimaki@gmail.com>
2023-02-13 20:00:32 +0200

    implemented AsyncStorage to save user data. Changes in UserInfoScreen now save the data. User data is accessible globally in the application. UserInfoScreen displays updated user data. Added new util function for retvieving user data. Changed the tab "tailored" to "sinulle"

--- screens/UserInfoScreen.js
+++ screens/UserInfoScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import * as userData from '../assets/data/userData.json'
 import {
   View,
@@ -7,17 +7,76 @@ import {
   ScrollView,
   KeyboardAvoidingView,
   TouchableWithoutFeedback,
+  Button,
 } from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import Slider from '@react-native-community/slider'
 import * as Colors from '../assets/styles/colors.js'
 import styles from '../assets/styles/styles'
 
+
+
 function UserInfoScreen() {
+  delete userData['default']
 
-  // const [myText, setMyText] = useState(20);        
+  const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState(userData)
 
+  useEffect(() => {
+    async function storeData() {
+      try {
+        await AsyncStorage.setItem('user', JSON.stringify(userData))
+        const newUser = await AsyncStorage.getItem('user')
+        setUser(JSON.parse(newUser))
+        setLoading(false)
+      } catch (e) {
+        // saving error
+      }
+    }
+    storeData()
+  },[])
+
+
+  // const [myText, setMyText] = useState(20);
+
+  const getData = async () => {
+    try {
+      const value = await AsyncStorage.getItem('user')
+      if(value !== null) {
+        console.log(JSON.parse(value))
+      }
+    } catch(e) {
+      // error reading value
+    }
+  }
+
+  const handleChange = async (event, key, subKey) => {
+    try {
+      const newUser = {...user}
+      if(subKey){
+        delete newUser[key][subKey]
+        newUser[key][subKey] = event
+      } else {
+        const value = event.nativeEvent.text
+        delete newUser[key]
+        newUser[key] = value
+      }
+      await AsyncStorage.setItem('user', JSON.stringify(newUser))
+      setUser(newUser)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  if(loading){
+    return(
+      <View>
+        <Text>Loading...</Text>
+      </View>
+    )
+  }
+  
   return (
-
     <KeyboardAvoidingView
       style={styles.userContainer}
     //behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
@@ -25,9 +84,10 @@ function UserInfoScreen() {
       <TouchableWithoutFeedback>
         <ScrollView style={[styles.userContent, styles.blackText]}>
           <View>
+            <Button title='log user' onPress={() => getData()} />
             <Text>
               <Text style={styles.h1}>Moi,</Text>
-              <Text style={[styles.h1, { color: Colors.krBlue, fontFamily: 'Inter-DisplayExtraBold' }]}> {userData.firstname}!</Text>
+              <Text style={[styles.h1, { color: Colors.krBlue, fontFamily: 'Inter-DisplayExtraBold' }]}> {user.firstname}!</Text>
             </Text>
             <Text style={{ textAlign: 'center' }}>Mitä työtä mielesi tekee?</Text>
           </View>
@@ -47,7 +107,8 @@ function UserInfoScreen() {
               minimumTrackTintColor={Colors.krGreen}
               maximumTrackTintColor={Colors.krGreen}
               step={1}
-              value={userData.preferences.morning}
+              value={user.preferences.morning}
+              onSlidingComplete={(event) => handleChange(event, 'preferences', 'morning')}
             />
             <View style={styles.tag}>
               <Text style={styles.label}>
@@ -59,7 +120,8 @@ function UserInfoScreen() {
               maximumValue={5}
               minimumValue={1}
               step={1}
-              value={userData.preferences.evening}
+              value={user.preferences.evening}
+              onSlidingComplete={(event) => handleChange(event, 'preferences', 'evening')}
             />
             <View style={styles.tag}>
               <Text style={styles.label}>
@@ -71,7 +133,8 @@ function UserInfoScreen() {
               maximumValue={5}
               minimumValue={1}
               step={1}
-              value={userData.preferences.night}
+              value={user.preferences.night}
+              onSlidingComplete={(event) => handleChange(event, 'preferences', 'night')}
             />
             <View style={styles.tag}>
               <Text style={styles.label}>
@@ -83,7 +146,8 @@ function UserInfoScreen() {
               maximumValue={5}
               minimumValue={1}
               step={1}
-              value={userData.preferences.pay}
+              value={user.preferences.pay}
+              onSlidingComplete={(event) => handleChange(event, 'preferences', 'pay')}
             />
             <View style={styles.tag}>
               <Text style={styles.label}>
@@ -95,7 +159,8 @@ function UserInfoScreen() {
               maximumValue={5}
               minimumValue={1}
               step={1}
-              value={userData.preferences.fullShift}
+              value={user.preferences.fullShift}
+              onSlidingComplete={(event) => handleChange(event, 'preferences', 'fullShift')}
             />
           </View>
 
@@ -104,17 +169,15 @@ function UserInfoScreen() {
               Etäisyys
             </Text>
             <Text style={styles.currentDistance}>
-              {userData.preferences.distance} km
+              {user.preferences.distance} km
             </Text>
 
             <Slider
               maximumValue={300}
               minimumValue={1}
               step={1}
-              value={userData.preferences.distance}
-            //   onValueChange = {(val) => currentDistance = val          }
-            // onValueChange={val => distance = updateDistance(val)}
-            //  value={userData.preferences.distance}
+              value={user.preferences.distance}
+              onSlidingComplete={(event) => handleChange(event, 'preferences', 'distance')}
             />
 
             <View style={styles.distanceSlider}>
@@ -131,63 +194,71 @@ function UserInfoScreen() {
             <TextInput
               editable
               style={styles.input}
-              placeholder={userData.firstname}
+              placeholder={user.firstname}
+              onEndEditing={(e) => handleChange(e, 'firstname')}
             />
             <Text style={styles.textfieldlist}>Sukunimi</Text>
             <TextInput
               editable
               style={styles.input}
-              placeholder={userData.lastname}
+              placeholder={user.lastname}
+              onEndEditing={(e) => handleChange(e, 'lastname')}
             />
             <Text style={styles.textfieldlist}>Sähköpostiosoite</Text>
             <TextInput
               editable
               style={styles.input}
-              placeholder={userData.email}
+              placeholder={user.email}
               keyboardType="email-address"
+              onEndEditing={(e) => handleChange(e, 'email')}
             />
             <Text style={styles.textfieldlist}>Puhelinnumero</Text>
             <TextInput
               editable
               style={styles.input}
-              placeholder={userData.phoneNumber}
+              placeholder={user.phoneNumber}
               keyboardType="phone-pad"
+              onEndEditing={(e) => handleChange(e, 'phoneNumber')}
             />
             <Text style={styles.textfieldlist}>Katuosoite</Text>
             <TextInput
               editable
               style={styles.input}
-              placeholder={userData.adress}
+              placeholder={user.adress}
+              onEndEditing={(e) => handleChange(e, 'address')}
             />
             <Text style={styles.textfieldlist}>Postinumero</Text>
             <TextInput
               editable
               style={styles.input}
-              placeholder={userData.postNumber}
+              placeholder={user.postNumber}
               keyboardType="numeric"
+              onEndEditing={(e) => handleChange(e, 'postNumber')}
             />
             <Text style={styles.textfieldlist}>Kunta</Text>
             <TextInput
               editable
               style={styles.input}
-              default={userData.city}
+              default={user.city}
+              onEndEditing={(e) => handleChange(e, 'city')}
             />
             <Text style={styles.textfieldlist}>Henkilötunnus</Text>
             <TextInput
               editable
               style={styles.input}
-              default={userData.personNumber}
+              default={user.personNumber}
+              onEndEditing={(e) => handleChange(e, 'personNumber')}
             />           
             <Text style={styles.textfieldlist}>Valviran rekisteröintinumero</Text>
             <TextInput
               editable
               style={styles.input}
-              default={userData.valviraID}
+              default={user.valviraID}
               keyboardType="numeric"
+              onEndEditing={(e) => handleChange(e, 'valviraID')}
             />
           </View>
           <Text style={styles.textfieldlist}></Text>
-
         </ScrollView>
       </TouchableWithoutFeedback>
     </KeyboardAvoidingView >
