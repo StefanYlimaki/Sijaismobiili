56796a14b259610e4e182f352c5b7922b0c18713
Jaakko Rantamäki <rantamaki@live.fi>
2023-03-22 17:22:47 +0200

    Switch to native PopUp

--- screens/UserInformation/UserInformationStack.js
+++ screens/UserInformation/UserInformationStack.js
@@ -2,6 +2,9 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import UserInfoScreen from './UserInfoScreen'
 import SettingsScreen from './SettingsScreen'
 import UserTabs from './UserTabs'
+import {useContext} from 'react'
+import {LocaleContext} from '../../App'
+import {Alert} from 'react-native'
 
 
 const UserInfoStack = createNativeStackNavigator()
@@ -11,21 +14,7 @@ export function UserInformationStack() {
     <UserInfoStack.Navigator>
       <UserInfoStack.Screen name='User' component={UserTabs} options={{ headerShown: false }}/>
       <UserInfoStack.Screen name='Settings' component={SettingsScreen} options={{ headerShown: true, title: 'Asetukset' }} />
-      <UserInfoStack.Screen name='RemoveAccountPopup' component={RemoveAccountPopup} options={{ presentation: 'transparentModal', headerShown: false }}/>
     </UserInfoStack.Navigator>
   )
 }
 
-function RemoveAccountPopup({navigation}) {
-  const { i18n } = useContext(LocaleContext)
-  
-  return (
-    <PopupDialog headerText={i18n.t('deleteConfirmation')} headerColor={Colors.danger} navigation={navigation}>
-      <View style={{justifyContent: 'center', alignItems: 'center', paddingTop: 25}}>
-        <Pressable style={styles.removeAccountButton}>
-          <Text style={styles.removeAccountButtonText}>{i18n.t('removeAccount')}</Text>
-        </Pressable>
-      </View>
-    </PopupDialog>
-  )
-}
