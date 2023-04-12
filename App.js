13a4b8794d9d776371b7ea546d3f6afe7cda8215
Roope Kipinä <35376295+Toopemus@users.noreply.github.com>
2023-04-12 20:49:16 +0300

    Revert "Added UpcomingGigsList import to app.js"

--- App.js
+++ App.js
@@ -19,7 +19,6 @@ import { fi, se, en } from './assets/data/localisation/localisations'
 import { LocaleContext } from './contexts/LocaleContext'
 import { UserInformationStack } from './screens/UserInformation/UserInformationStack'
 import SubstitutionCard from './components/SubstitutionCard'
-import UpcomingGigsList from './components/UpcomingGigsList'
 
 const Stack = createNativeStackNavigator()
 
