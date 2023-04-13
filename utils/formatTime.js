412b5807cc57302f283355cb7587270f0100db5e
RoopeK <35376295+Toopemus@users.noreply.github.com>
2023-04-13 16:50:20 +0300

    Added fallbacks to date and time format on android
    Might revisit this to implement a proper solution

--- utils/formatTime.js
+++ utils/formatTime.js
@@ -7,5 +7,11 @@ export function formatTime(timestamp, duration) {
   const startTime = new Date(timestamp)
   const endTime = new Date(startTime.getTime() + duration*60000)
 
-  return startTime.toLocaleTimeString(locale, {hour: '2-digit', minute: '2-digit'}) + '-' + endTime.toLocaleTimeString(locale, {hour: '2-digit', minute: '2-digit'})
+  // Can't use toLocaleTimeString on android due to ancient JavaScriptCore, therefore:
+  if (Platform.OS === 'android') {
+    // Fallback
+    return startTime.getHours().toString().padStart(2, '0') + '.' + startTime.getMinutes().toString().padStart(2, '0') + '-' + endTime.getHours().toString().padStart(2, '0') + '.' + endTime.getMinutes().toString().padStart(2, '0')
+  } else {
+    return startTime.toLocaleTimeString(locale, {hour: '2-digit', minute: '2-digit'}) + '-' + endTime.toLocaleTimeString(locale, {hour: '2-digit', minute: '2-digit'})
+  }
 }
