85b585f0dae9080b99f7f2309528e48e695caaa0
Jasmin Leppiniemi <jasmin.leppiniemi@gmail.com>
2023-05-03 20:07:04 +0300

    korjailin

--- screens/MainApplication/OwnSubstitutionsScreen.js
+++ screens/MainApplication/OwnSubstitutionsScreen.js
@@ -7,9 +7,11 @@ import UpcomingGigs from '../../components/UpcomingGigs'
 import { getUserData } from '../../utils'
 import NavigateToSavedSubstitutionsButton from '../../components/NavigateToSavedSubstitutionsButton'
 import Payslip from '../../components/Payslip'
-
+import {getCalendar} from '../../utils/getCalendar.js'
+import { LocaleContext } from '../../contexts/LocaleContext'
 
 const OwnSubstitutionsScreen = ({ navigation }) => {
+  const { i18n, locale, setLocale } = useContext(LocaleContext)
 
   const [isMonth, setIsMonth] = React.useState(true)
   const [userSubstitutions, setUserSubstitutions] = useState()
@@ -31,47 +33,21 @@ const OwnSubstitutionsScreen = ({ navigation }) => {
   return (
     <ScrollView contentContainerStyle={{paddingBottom: 300}}>
       <NavigateToSavedSubstitutionsButton navigation={navigation}/>
-      <View style ={Styles.calendar}>
-        <Calendar 
-          style ={Styles.calendar}
-          headerStyle={{paddingTop: 5}}
-          firstDay = {1}
-          markingType={'custom'}
-          markedDates={{
-            selected: {
-              customStyles: {
-                container: {
-                  backgroundColor: 'green'
-                },
-                text: {
-                  color: 'black',
-                  fontWeight: 'bold'
-                }
-              }
-            },
-          }}
-          onDayPress={() => setSelected(selected)}
-          theme={{
-            backgroundColor: '#ffffff',
-            calendarBackground: '#ffffff',
-            textSectionTitleColor: '#b6c1cd',
-            selectedDayBackgroundColor: '#00adf5',
-            selectedDayTextColor: Colors.krGreen,
-            dayTextColor: '#2d4150',
-            textDisabledColor: '',
-          }}   
-        />
+
+      <View style={{ flexDirection: "row", justifyContent: 'space-between' }}>
+        <Pressable onPress={() => setIsMonth(true)}><Text>{i18n.t('month')}</Text></Pressable>
+        <Pressable onPress={() => setIsMonth(false)}><Text>{i18n.t('week')}</Text></Pressable>
       </View>
 
       {getCalendar(isMonth)}
 
       <View style={{ paddingHorizontal: '5%' }}>
         <View>
-          <Text style={[Styles.h2, {textAlign: 'left', marginLeft: 25}]}>{i18n.t('nextShift')}</Text>
+          <Text style={[Styles.h2, {textAlign: 'left'}]}>{i18n.t('nextShift')}</Text>
           {userSubstitutions && <UpcomingGigs substIDs={userSubstitutions} navigation={navigation} />}
         </View>
 
-        <Text style={[Styles.h2, {textAlign: 'left', marginLeft: 25}]}>Palkkakuitti</Text>
+        <Text style={[Styles.h2, {textAlign: 'left'}]}>Palkkakuitti</Text>
         <Payslip navigation={navigation}/>
       </View>
     </ScrollView>
